4ae5b366be75087a778e073908897bc9042912f0
Carla Holcomb <carla.programmer@gmail.com>
2020-06-15 01:36:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
    function mapToNegativize(sourceArray) {
   let r = []
   for (let i = 0; i < sourceArray.length; i++ ) {
-    r.push(-1 * src[i])
+    r.push(-1 * sourceArray[i])
   }
   return r
 }
