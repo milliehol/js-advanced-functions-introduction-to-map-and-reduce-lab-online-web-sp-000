74b2cd6a019218392440a39d05cbd32bbf314399
Carla Holcomb <carla.programmer@gmail.com>
2020-06-15 01:37:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,48 +8,48 @@
   return r
 }
 
-function mapToNoChange(src) {
+function mapToNoChange(sourceArray) {
   let r = []
-  for (let i = 0; i < src.length; i++ ) {
-    r.push(src[i])
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(sourceArray[i])
   }
   return r
 }
 
-function mapToDouble(src) {
+function mapToDouble(sourceArray) {
   let r = []
-  for (let i = 0; i < src.length; i++ ) {
-    r.push(2 * src[i])
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(2 * sourceArray[i])
   }
   return r
 }
 
-function mapToSquare(src) {
+function mapToSquare(sourceArray) {
   let r = []
-  for (let i = 0; i < src.length; i++ ) {
-    r.push(src[i] * src[i])
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(sourceArray[i] * sourceArray[i])
   }
   return r
 }
 
-function reduceToTotal(src, startingPoint=0) {
+function reduceToTotal(sourceArray, startingPoint=0) {
   let total = startingPoint
-  for (let i = 0; i < src.length; i++ ) {
-    total = total + src[i]
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    total = total + sourceArray[i]
   }
   return total
 }
 
-function reduceToAllTrue(src) {
-  for (let i = 0; i < src.length; i++ ) {
-    if (!src[i]) return false
+function reduceToAllTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    if (!sourceArray[i]) return false
   }
   return true
 }
 
-function reduceToAnyTrue(src) {
-  for (let i = 0; i < src.length; i++ ) {
-    if (src[i]) return true
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    if (sourceArray[i]) return true
   }
   return false
 }
